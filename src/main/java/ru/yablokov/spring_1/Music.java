1e1489cd8f4c79f2e7d63533183f7036e5de5156
Daniil34525 <danilnaster@gmail.com>
2024-02-12 22:14:26 +0300

    4 lesson

---
+++ src/main/java/ru/yablokov/spring_1/Music.java
@@ -0,0 +1,5 @@
+package ru.yablokov.spring_1;
+
+public interface Music {
+	String getSong();
+}
---
+++ src/main/java/ru/yablokov/spring_1/RockMusic.java
@@ -0,0 +1,10 @@
+package ru.yablokov.spring_1;
+
+public class RockMusic implements Music{
+
+	@Override
+	public String getSong() {
+		return "Wind cries Mary";
+	}
+	
+}
