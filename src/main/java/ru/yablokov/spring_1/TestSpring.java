2dc1a71d3398bbc92e49bf0701f4b09791748238
Daniil34525 <danilnaster@gmail.com>
2024-02-12 20:11:31 +0300

    3 lesson

---
+++ src/main/java/ru/yablokov/spring_1/TestSpring.java
@@ -0,0 +1,15 @@
+package ru.yablokov.spring_1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		TestBean testBean = context.getBean("testBean", TestBean.class);
+		
+		System.out.print(testBean.getName());
+		
+		context.close();
+	}
+}
